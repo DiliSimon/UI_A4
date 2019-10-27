a055f6a847a151be9d3b757257243018604cf5ed
Simon <Gtingwen@outlook.com>
2019-10-27 19:55:35 -0400

    Setting Activity implemented. TODO: Enforce max withdrawl; Cleanup.

--- app/src/main/java/com/uima/joanne/navfragments/SettingActivity.java
+++ app/src/main/java/com/uima/joanne/navfragments/SettingActivity.java
@@ -1,5 +1,7 @@
 package com.uima.joanne.navfragments;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,6 +11,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SettingActivity extends AppCompatActivity {
 
@@ -19,6 +24,31 @@ public class SettingActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-    }
+        final SharedPreferences sharedPref = this.getSharedPreferences("setting", Context.MODE_PRIVATE);
 
+        Button confirm = (Button) findViewById(R.id.setting_confirm);
+        Button cancel = (Button) findViewById(R.id.setting_cancel);
+        final EditText min_balance = (EditText) findViewById((R.id.editText2));
+        final EditText max_withdrawl = (EditText) findViewById(R.id.max_withdrawl);
+
+        confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor editor = sharedPref.edit();
+                int min = Integer.parseInt(min_balance.getText().toString());
+                int max = Integer.parseInt(max_withdrawl.getText().toString());
+                editor.putInt("min_balance", min);
+                editor.putInt("max_withdral", max);
+                editor.commit();
+                finish();
+            }
+        });
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
 }
